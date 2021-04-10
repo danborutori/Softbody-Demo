5e67fbb93e8a41c8b834e1097358d064d2305cec
anmc <lamylanmc@hotmail.com>
2021-04-10 10:17:36 +0800

    update version update build

--- ts/src/config.ts
+++ ts/src/config.ts
@@ -1,7 +1,7 @@
 namespace hahaApp {
 
     export const config = {
-        version: "1.0.4",
+        version: "1.0.5",
         author: "ANMCダンボールとり",
         projectUrl: "https://github.com/danborutori/Softbody-Demo"
     }
